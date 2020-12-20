a57fc9dd628d9028a54cf3433ab5810bf918a108
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-20 12:35:10 +0100

    🔧 use typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Head from "next/head";
+import Nav from "components/nav";
 
 const Home = () => (
   <div>
@@ -83,6 +83,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
