8c63a081fe03a2a25063c4abbb728bb6549b53a4
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-26 11:00:03 +0100

    ✨ add list query

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,21 @@
 import getFriends from "functions/getFriends";
-import { Resolvers } from "../../__generated__/src/graphql/type-defs.graphqls";
+import {
+  Resolvers,
+  User,
+} from "../../__generated__/src/graphql/type-defs.graphqls";
 import { Context } from "./context";
+
+const formatTwitterUser = ({
+  id_str,
+  name,
+  screen_name,
+  profile_image_url,
+}): User => ({
+  name,
+  screenName: screen_name,
+  imageSrc: profile_image_url,
+  id: id_str,
+});
 
 const resolvers: Resolvers<Context> = {
   Query: {
@@ -15,15 +30,31 @@ const resolvers: Resolvers<Context> = {
         ...(!!cursor && { cursor }),
       });
 
+      return Promise.all(friends.users.map(formatTwitterUser));
+    },
+    lists: async (_, { ownership }, { twitterClient }) => {
+      const {
+        screen_name,
+      } = await twitterClient.accountsAndUsers.accountSettings();
+      const lists = await twitterClient.accountsAndUsers.listsList({
+        ...(ownership === "OWNED" && {
+          screen_name,
+        }),
+      });
       return Promise.all(
-        friends.users.map(
-          ({ id_str, name, screen_name, profile_image_url, description }) => ({
-            name,
-            screenName: screen_name,
-            imageSrc: profile_image_url,
+        lists
+          .filter(({ user: { screen_name: listScreenName } }) =>
+            ownership === "OWNED" ? screen_name === listScreenName : true
+          )
+          .map(({ description, name, full_name, uri, id_str, slug, user }) => ({
             id: id_str,
-          })
-        )
+            name,
+            fullName: full_name,
+            description,
+            slug,
+            uri,
+            user: formatTwitterUser(user),
+          }))
       );
     },
   },
