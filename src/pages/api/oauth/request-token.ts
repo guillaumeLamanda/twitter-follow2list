f1795be0bec8dc4e8455c263ea20109bbce93847
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-27 14:38:17 +0100

    add /api/oauth/request-token route

---
+++ src/pages/api/oauth/request-token.ts
@@ -0,0 +1,33 @@
+import oauth from "lib/oauth";
+import getRedisClient from "lib/redis";
+import { NextApiRequest, NextApiResponse } from "next";
+import { promisify } from "util";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  return new Promise((resolve, reject) => {
+    oauth.getOAuthRequestToken(async (err, oauthToken, oauthTokenSecret) => {
+      if (err) {
+        res.status(500);
+        resolve(err);
+      }
+      const redisClient = getRedisClient();
+      redisClient.on("error", (error) => {
+        console.error(error);
+        if (!res.headersSent) {
+          res.status(500).end();
+          resolve(error);
+        }
+      });
+      redisClient.set(oauthToken, oauthTokenSecret, () => {
+        const twoMinutesInSecond = 60 * 2;
+        redisClient.expire(oauthToken, twoMinutesInSecond);
+        redisClient.quit();
+
+        res.status(200).json({
+          oauthToken,
+        });
+        resolve(oauthToken);
+      });
+    });
+  });
+};
