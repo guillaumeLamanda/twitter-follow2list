6f3d154149d47b19d5da1367713e99805d02de2b
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-27 14:38:31 +0100

    add /api/oauth/callback route

---
+++ src/pages/api/oauth/callback.ts
@@ -0,0 +1,59 @@
+import oauth from "lib/oauth";
+import getRedisClient from "lib/redis";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
+export default (
+  { query: { oauth_token, oauth_verifier } }: NextApiRequest,
+  res: NextApiResponse
+) => {
+  return new Promise((resolve) => {
+    const errCb = (err) => {
+      if (!res.headersSent) res.status(500).end();
+      redisClient.quit();
+      resolve(err);
+    };
+
+    const redisClient = getRedisClient();
+
+    redisClient.on("error", errCb);
+
+    redisClient.get(oauth_token as string, (err, oauthTokenSecret) => {
+      if (err) {
+        errCb(err);
+      }
+      oauth.getOAuthAccessToken(
+        oauth_token as string,
+        oauthTokenSecret,
+        oauth_verifier as string,
+        (err, token, tokenSecret) => {
+          if (err) {
+            errCb(err);
+          }
+
+          redisClient.del(oauth_token, () => {
+            redisClient.hmset(
+              oauth_token as string,
+              "token",
+              token,
+              "secret",
+              tokenSecret,
+              () => {
+                const twoHoursInSeconds = 60 * 60 * 2;
+                redisClient.expire(oauth_token as string, twoHoursInSeconds);
+                redisClient.quit();
+                res.redirect(`/app`);
+                resolve(oauth_token);
+              }
+            );
+          });
+        }
+      );
+    });
+  });
+};
