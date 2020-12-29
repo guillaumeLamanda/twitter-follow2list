fb37a6806c9e86c3537f73eb10833489fb1e452d
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-29 12:44:37 +0100

    add callbackUrl to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 export const apiKey = process.env.API_KEY;
 export const apiKeySecret = process.env.API_KEY_SECRET;
 export const bearerToken = process.env.BEARER_TOKEN;
+export const callbackUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://twitter.follow2list.lamanda.fr/api/oauth/callback"
+    : "http://localhost:3000/api/oauth/callback";
