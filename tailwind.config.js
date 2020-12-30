ae47e442d8e132d567a46b48b4fa85c479059bb6
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-30 22:36:23 +0100

    add viewport tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // 'media' or 'class'
   theme: {
+    maxHeight: {
+      '25-vh': '25vh',
+      '50-vh': '50vh',
+      '75-vh': '75vh',
+      '80-vh': '80vh',
+      '90-vh': '90vh',
+    },
     extend: {},
   },
   variants: {
