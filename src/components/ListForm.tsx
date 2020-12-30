a2f812f68b10743ff7ddc552a7d8537e4df0f6fd
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-30 22:42:21 +0100

    WIP create List

--- src/components/ListForm.tsx
+++ src/components/ListForm.tsx
@@ -5,6 +5,7 @@ import {
   ListQuery,
   ListQueryVariables,
 } from "graphql/queries/list.graphql";
+import { useCallback } from "react";
 import ListDescriptionInput from "./ListDescriptionInput";
 import ListTitleInput from "./ListTitleInput";
 
@@ -14,62 +15,91 @@ type ListFormProps = {
   name: string;
   description: string;
   onSubmit: () => void;
+  onCancel: () => void;
 };
-const ListForm = ({ id, slug, name, description, onSubmit }: ListFormProps) => {
-  const [updateList] = useUpdateListMutation();
+
+// export function ListFormUpdate({ id, slug, name, description }: ListFormProps) {
+//   const [updateList] = useUpdateListMutation();
+
+//   const onSubmit = useCallback(async (values) => {
+//     updateList({
+//       variables: {
+//         input: {
+//           id,
+//           slug,
+//           description: values.description,
+//           title: values.name,
+//         },
+//       },
+//       optimisticResponse: ({ input }) => ({
+//         updateList: {
+//           ...input,
+//           name: input.title || name,
+//           description: input.description || description,
+//           __typename: "UserList",
+//         },
+//         __typename: "Mutation",
+//       }),
+//       update: (cache, result) => {
+//         if (result.errors?.length) return;
+//         cache.writeQuery<ListQuery, ListQueryVariables>({
+//           query: ListDocument,
+//           variables: {
+//             id,
+//             slug,
+//           },
+//           data: {
+//             list: result.data?.updateList,
+//             __typename: "Query",
+//           },
+//         });
+//       },
+//     });
+//   }, []);
+
+//   return <ListForm onSubmit={onSubmit} onCancel name description />;
+// }
+
+const ListForm = ({
+  id,
+  slug,
+  name,
+  description,
+  onSubmit,
+  onCancel,
+}: ListFormProps) => {
   const form = useFormik<{ name: string; description: string }>({
     initialValues: {
       name,
       description,
     },
-    onSubmit: async (values) => {
-      updateList({
-        variables: {
-          input: {
-            id,
-            slug,
-            description: values.description,
-            title: values.name,
-          },
-        },
-        optimisticResponse: ({ input }) => ({
-          updateList: {
-            ...input,
-            name: input.title || name,
-            description: input.description || description,
-            __typename: "UserList",
-          },
-          __typename: "Mutation",
-        }),
-        update: (cache, result) => {
-          if (result.errors?.length) return;
-          console.log({ result });
-          cache.writeQuery<ListQuery, ListQueryVariables>({
-            query: ListDocument,
-            variables: {
-              id,
-              slug,
-            },
-            data: {
-              list: result.data?.updateList,
-              __typename: "Query",
-            },
-          });
-        },
-      });
-      onSubmit();
-    },
+    onSubmit,
   });
 
   return (
     <FormikProvider value={form}>
-      <div className="space-y-1">
-        <ListTitleInput name="name" />
-        <ListDescriptionInput name="description" />
+      <div className="space-x-2 inline-flex">
+        <div className="space-y-1">
+          <ListTitleInput name="name" />
+          <ListDescriptionInput name="description" />
+        </div>
+        <div className="flex flex-col space-y-2 justify-around">
+          <button
+            className="py-1 px-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2"
+            disabled={form.isSubmitting}
+            onClick={form.submitForm}
+          >
+            save
+          </button>
+          <button
+            className="py-1 px-2 focus:ring-red-500 focus:ring-offset-red-200 text-white hover:text-pink-600 w-full transition ease-in duration-200 text-center text-base font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2"
+            disabled={form.isSubmitting}
+            onClick={onCancel}
+          >
+            cancel
+          </button>
+        </div>
       </div>
-      <button disabled={form.isSubmitting} onClick={form.submitForm}>
-        save
-      </button>
     </FormikProvider>
   );
 };
--- src/components/ListList.tsx
+++ src/components/ListList.tsx
@@ -1,13 +1,33 @@
 import { useListsQuery } from "graphql/queries/lists.graphql";
+import { useState } from "react";
+import ListForm from "./ListForm";
 import ListListItem from "./ListListItem";
+
+function AddList() {
+  const [editing, setEditing] = useState(false);
+
+  // if(editing) return <ListForm />
+
+  return (
+    <button
+      className="primary-button"
+      onClick={() => {
+        setEditing(true);
+      }}
+    >
+      create new list
+    </button>
+  );
+}
 
 export default function ListList() {
   const { data } = useListsQuery();
   return (
-    <ul className="overflow-scroll">
+    <ul>
       {data?.lists?.map(({ id, slug }) => (
         <ListListItem key={id} id={id} slug={slug} />
       ))}
+      <AddList />
     </ul>
   );
 }
