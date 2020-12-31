6d91699958f0f0b05e03eee67e7dbe51fb7f8a1b
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-12-31 19:09:41 +0100

    connect createList mutation

--- src/components/ListFormAdd.tsx
+++ src/components/ListFormAdd.tsx
@@ -1,4 +1,10 @@
 import { FormikValues } from "formik";
+import { useCreateListMutation } from "graphql/mutations/createList.graphql";
+import {
+  ListsDocument,
+  ListsQuery,
+  ListsQueryVariables,
+} from "graphql/queries/lists.graphql";
 import { useState } from "react";
 import ListForm from "./ListForm";
 
@@ -6,9 +12,32 @@ export function ListFormAdd() {
   const [editing, setEditing] = useState(false);
 
   const hideForm = () => setEditing(false);
+  const [createList] = useCreateListMutation({
+    update: (cache, result) => {
+      if (!result.data) return;
+      const data = cache.readQuery<ListsQuery, ListsQueryVariables>({
+        query: ListsDocument,
+      });
 
-  const onSubmit = (values: FormikValues) => {
-    console.log(values);
+      cache.writeQuery<ListsQuery, ListsQuery>({
+        query: ListsDocument,
+        data: {
+          __typename: "Query",
+          lists: [result.data.createList, ...data.lists],
+        },
+      });
+    },
+  });
+
+  const onSubmit = async (values: FormikValues) => {
+    await createList({
+      variables: {
+        input: {
+          title: values.name,
+          description: values.description,
+        },
+      },
+    });
   };
 
   if (editing)
