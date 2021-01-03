8ea4c2a84c44a9e731ffcc88dfe2aa414616d343
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2021-01-03 17:07:01 +0100

    update merge strategy

--- src/graphql/cache/index.ts
+++ src/graphql/cache/index.ts
@@ -6,12 +6,25 @@ export default new InMemoryCache({
       fields: {
         friends: {
           keyArgs: false,
-          merge: (existing, incoming) => {
+          merge: (existing, incoming, { readField }) => {
             if (!existing) return incoming;
+
+            const firstIncomingNodeId = readField("id", incoming.nodes[0]);
+            const offset = existing.nodes.some(
+              (item) => readField("id", item) === firstIncomingNodeId
+            )
+              ? existing.nodes.indexOf(firstIncomingNodeId)
+              : existing.nodes.length;
+
+            const mergedNodes = existing.nodes.slice(0);
+
+            for (let i = 0; i < incoming.nodes.length; ++i) {
+              mergedNodes[offset + i] = incoming.nodes[i];
+            }
 
             return {
               ...incoming,
-              nodes: [...existing.nodes, ...incoming.nodes],
+              nodes: mergedNodes,
             };
           },
         },
